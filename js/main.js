a07532f6c5a6f7faa8abde3b6d82580987c85c2b
Yoni Mor <yonimor89@hotmail.co.il>
2014-12-02 15:45:13 +0200

    bug fix

--- js/main.js
+++ js/main.js
@@ -1,70 +1,73 @@
 var mq = window.matchMedia("(min-width: 1004px)");
+var links = document.querySelectorAll('[am-sliderBullets] a');
+
+var slider = document.querySelector('[am-layoutWrap~="slider"]');
+
+var getActivatedIndex = function(links) {
+	for (var i = 0; i < links.length; i++) {
+		if (links[i].hasAttribute('am-marked')) {
+			return i;
+		}
+	}
+};
+
+var displaySlide = function() {
+	var slides = document.querySelectorAll('[am-wrapSlideContent]');
+	for (var i = 0; i < slides.length; i++) {
+		if (i === getActivatedIndex(links)) {
+			slides[i].style.display = 'block';
+		}
+		else {
+			slides[i].style.display = 'none';
+		}
+	}
+};
+
+var changeSlide = function(e) {
+	e.preventDefault();
+	var target = e.target;
+	target.setAttribute('am-marked', '');
+	for (var i = 0; i < links.length; i++) {
+		if (links[i] !== target && links[i].hasAttribute('am-marked')) {
+			links[i].removeAttribute('am-marked');
+		}
+	}
+	displaySlide();
+};
+
+var hammertime = new Hammer(slider);
+hammertime.on('swiperight', function(ev) {
+    console.log(ev);
+    // If swipe right, getActivatedIndex and click() on the next bullet.
+    var active = getActivatedIndex(links);
+    console.log(active);
+    if (active < 3) {
+	    active = active + 1;
+	}
+	else {
+		active = 0;
+	}
+	links[active].click();
+});
+
+hammertime.on('swipeleft', function(ev) {
+    console.log(ev);
+    // If swipe left, getActivatedIndex and click() on the previous bullet.
+    var active = getActivatedIndex(links);
+    console.log(active);
+    if (active > 0) {
+	    active = active -1;
+	}
+	else {
+		active = 3;
+	}
+	links[active].click();
+});
 
 var mqLaunch = function(mq) {
 	if (mq.matches) {
-
-		var links = document.querySelectorAll('[am-sliderBullets] a');
-
-	    /* the view port is at least 1004 pixels wide */
-	    var slider = document.querySelector('[am-layoutWrap~="slider"]');
-	    var hammertime = new Hammer(slider);
-		hammertime.on('swiperight', function(ev) {
-		    console.log(ev);
-		    // If swipe right, getActivatedIndex and click() on the next bullet.
-		    var active = getActivatedIndex(links);
-		    if (active < 3) {
-			    active = active + 1;
-			}
-			else {
-				active = 0;
-			}
-			links[active].click();
-		});
-
-		hammertime.on('swipeleft', function(ev) {
-		    console.log(ev);
-		    // If swipe left, getActivatedIndex and click() on the previous bullet.
-		    var active = getActivatedIndex(links);
-		    if (active > 0) {
-			    active = active -1;
-			}
-			else {
-				active = 3;
-			}
-			links[active].click();
-		});
-
-		var getActivatedIndex = function(links) {
-			for (var i = 0; i < links.length; i++) {
-				if (links[i].hasAttribute('am-marked')) {
-					return i;
-				}
-			}
-		};
-
-		var displaySlide = function() {
-			var slides = document.querySelectorAll('[am-wrapSlideContent]');
-			for (var i = 0; i < slides.length; i++) {
-				if (i === getActivatedIndex(links)) {
-					slides[i].style.display = 'block';
-				}
-				else {
-					slides[i].style.display = 'none';
-				}
-			}
-		};
-
-		var changeSlide = function(e) {
-			e.preventDefault();
-			var target = e.target;
-			target.setAttribute('am-marked', '');
-			for (var i = 0; i < links.length; i++) {
-				if (links[i] !== target && links[i].hasAttribute('am-marked')) {
-					links[i].removeAttribute('am-marked');
-				}
-			}
-			displaySlide();
-		};
+		/* the view port is at least 1004 pixels wide */
+		console.log('viewport 1004+');
 
 		for (var i = 0; i < links.length; i++) {
 			var link = links[i];
@@ -74,6 +77,8 @@ var mqLaunch = function(mq) {
 		displaySlide();
 	}
 	else {
+		// The viewport is less than 1004px wide.
+		console.log('viewport 1004-');
 		var slides = document.querySelectorAll('[am-wrapSlideContent]');
 		for (var j = 0; j < slides.length; j++) {
 			slides[j].style.display = 'block';
